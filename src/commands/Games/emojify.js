59be4d2e5fdff20b307042e804754fad4e541641
Fatih Simsek <154845930+Fatih5252@users.noreply.github.com>
2024-03-25 17:34:46 +0100

    Update emojify.js

--- src/commands/Games/emojify.js
+++ src/commands/Games/emojify.js
@@ -1,15 +1,31 @@
-const { Emojify } = require('discord-gamecord');
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('emojify')
-    .setDescription('Emojify your text!')
-    .addStringOption(option => option.setName('text').setDescription('The text you want to emojify').setRequired(true)),
+    .setDescription('change text to emojis')
+    .addStringOption(option => option.setName('text').setDescription('The text to convert').setRequired(true))
+    .addStringOption(option => option.setName('hidden').setDescription('Hide this message?').addChoices({ name: 'Hidden', value: 'true' }, { name: 'Not Hidden', value: 'false' }).setRequired(true)),
     async execute (interaction) {
 
-        const Text = interaction.options.getString('text');
+        const { options } = interaction;
+        const text = options.getString('text');
+        var hidden = options.getString('hidden') || false;
 
-        await interaction.reply(await Emojify(Text));
+        if (hidden === 'true') hidden = true;
+        else if (hidden === 'false') hidden = false;
+
+        var emojiText = text
+        .toLowerCase()
+        .split('')
+        .map((letter) => {
+            if (letter == ' ') return ' ';
+            else return `:regional_indicator_${letter}:`
+        })
+        .join('');
+
+        if (emojiText.length >= 2000) emojiText = `I cant emojify this text, It's too long!`;
+
+        await interaction.reply({ content: emojiText, ephemeral: hidden })
     }
 }
