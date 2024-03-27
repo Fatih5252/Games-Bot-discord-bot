9f9633601c9073727393ba6ce858f48bbed0668c
Fatih Simsek <154845930+Fatih5252@users.noreply.github.com>
2024-03-27 18:29:08 +0100

    ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,24 @@
+const mongoose = require('mongoose')
+const mongoURL = process.env.mongoURL
+
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
         console.log('Ready!');
+
+        if (!mongoURL) return;
+
+        await mongoose.connect(mongoURL || '', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        if (mongoose.connect) {
+            console.log('I have successfully connected to the mongoDB database!')
+        } else {
+            console.log('I couldnt connect to the mongoDB database!')
+        }
 
         async function pickPresence () {
             const option = Math.floor(Math.random() * statusArray.length);
